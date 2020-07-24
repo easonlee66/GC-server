3a2c4237e5b3e7c424ae8b2b90348975a86e8dc4
easonlee66 <1807365372@qq.com>
2020-07-24 19:14:54 +0800

    Update 一键更新.bat

--- 一键更新.bat
+++ 一键更新.bat
@@ -7,8 +7,13 @@ echo GC-server Upgrading,please wait……
 @echo off
 
 echo .
+rem 更新GC-server
 cd "%~dp0"\git\cmd\
+git reset --hard origin/master
 git pull origin master
+
+git gc
+git prune
 
 @echo on 
 echo GC-server Upgraded,enjoy it!
