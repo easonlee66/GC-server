f4ae9a10caab621740d8c385afdf58ff5349de29
easonlee66 <1807365372@qq.com>
2020-07-24 19:19:10 +0800

    Update 一键更新.bat

--- 一键更新.bat
+++ 一键更新.bat
@@ -6,14 +6,23 @@ echo GC-server Upgrading,please wait……
 
 @echo off
 
-echo .
-rem 更新GC-server
+IF EXIST "%~dp0"\.git\index.lock ( 
+	del /f /s /q "%~dp0"\.git\index.lock > nul
+	rd /q /s "%~dp0"\.git\index.lock > nul
+)
+
+echo.
+echo.
 cd "%~dp0"\git\cmd\
+rem 更新GC-server
 git reset --hard origin/master
 git pull origin master
 
 git gc
 git prune
+
+rem rd/s/q "%~dp0"\.git\refs\original > nul
+rem rd/s/q "%~dp0"\.git\logs\ > nul
 
 @echo on 
 echo GC-server Upgraded,enjoy it!
