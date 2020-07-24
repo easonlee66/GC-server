75823dd2fb938516a21c1ab1c9bbc2188cbf34f5
easonlee66 <1807365372@qq.com>
2020-07-24 18:17:24 +0800

    UPdate somefile

--- 一键更新.bat
+++ 一键更新.bat
@@ -1,6 +1,17 @@
 @echo off
 title GC-server Upgrader
+
+@echo on
 echo GC-server Upgrading,please wait……
 
+@echo off
+
 echo .
-cd "%~dp0"\Git\cmd\
+cd "%~dp0"\git\cmd\
+git pull origin master
+
+@echo on 
+echo GC-server Upgraded,enjoy it!
+
+@echo off
+pause
